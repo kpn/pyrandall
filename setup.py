d59c8e5390044e4e24014f2cd6697e98e637072f
Stefano Oldeman <stefano.oldeman@gmail.com>
2020-06-14 15:28:00 +0200

    fix: version fix requirements

--- setup.py
+++ setup.py
@@ -13,15 +13,15 @@ setup(
     url="https://github.com/kpn/pyrandall",
     entry_points={"console_scripts": ["pyrandall = pyrandall.cli:main"]},
     install_requires=[
-        "jsonschema",
-        "jsondiff",
+        "jsonschema~=3.2.0",
+        "jsondiff~=1.2.0",
         "requests~=2.20",
-        "PyYAML==5.1",
-        "Mako",
-        "curlify",
+        "PyYAML~=5.1",
+        "Mako~=1.1.3",
+        "curlify~=2.2.0",
         "confluent-kafka~=1.0",
         "deepdiff==3.3",
-        "pluggy",
+        "pluggy~=0.13.0"
     ],
     tests_require=["pytest", "pytest_httpserver", "responses", "vcrpy", "freezegun"],
 )
